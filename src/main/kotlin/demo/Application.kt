0889782fc037a9c9801575ec37b80cad5ddb8cc6
Minmin Qiu <mmqiu@thoughtworks.com>
2018-07-08 23:11:45 +0800

    add one test for kotlin function

--- src/main/kotlin/demo/Application.kt
+++ src/main/kotlin/demo/Application.kt
@@ -1,5 +1,9 @@
 package demo
 
 fun main(args: Array<String>) {
-    print("Hello World !")
+   checkIsTrue()
+}
+
+fun checkIsTrue() :Boolean {
+    return true
 }
---
+++ src/test/kotlin/demo/ApplicationKtTest.kt
@@ -0,0 +1,12 @@
+package demo
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class ApplicationKtTest {
+
+    @Test
+    fun testResultIsTrue() {
+        assertTrue(checkIsTrue())
+    }
+}
